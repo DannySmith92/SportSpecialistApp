499799c58ee44c60de26f106029a044844249ca2
Sincrono-Consulenza <sincrono.consulenza2020@gmail.com>
2024-06-20 18:15:40 +0200

    complete view - home, about, classes, service, our team, contact

---
+++ WebApp/Controllers/AboutUsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
